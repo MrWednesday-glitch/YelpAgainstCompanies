daa146ce4be3ca9b7fb515eb9f893726b6870d37
Rowan Chander <rowan.chander@itvitaelearning.nl>
2023-09-20 15:16:24 +0200

    Ran migration

--- YelpAgainstCompanies.Data/DataContext.cs
+++ YelpAgainstCompanies.Data/DataContext.cs
@@ -1,13 +1,43 @@
 ﻿using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace YelpAgainstCompanies.Data;
 
 public class DataContext : IdentityDbContext<IdentityUser, IdentityRole, string>
 {
+    public DataContext()
+    {
+        
+    }
+
     public DataContext(DbContextOptions<DataContext> options) : base(options)
     {
 
+    }
+
+    public DbSet<Company> Companies { get; set; } = null!;
+
+    public DbSet<Rating> Rating { get; set; } = null!;
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        var configBuilder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+        if (!optionsBuilder.IsConfigured)
+        {
+            optionsBuilder.UseSqlServer(configBuilder.GetConnectionString("DefaultConnection"));
+        }
+
+        base.OnConfiguring(optionsBuilder);
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
     }
 }
