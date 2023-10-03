80e3ee48d3fb32bdc6e3588bb58445356a6f44c2
Rowan Chander <chander89@live.nl>
2023-10-04 00:00:09 +0200

    Made a todo for the future

--- YelpAgainstCompanies.Business/Services/RatingService.cs
+++ YelpAgainstCompanies.Business/Services/RatingService.cs
@@ -1,5 +1,7 @@
 ﻿namespace YelpAgainstCompanies.Business.Services;
 
+//TODO Have adding a singular rating to a company update (CRUD) the average total rating of the company
+//If the total rating is null or zero have it become the first singular rating
 public class RatingService : IRatingService
 {
     private readonly IRatingRepository _ratingRepository;
