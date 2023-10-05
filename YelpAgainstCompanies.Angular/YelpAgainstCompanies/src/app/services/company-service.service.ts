c5ebe3d9ea2acd94224fe84fd806e1998ad9230d
Rowan Chander <chander89@live.nl>
2023-10-05 16:47:13 +0200

    Made a service to get the companies from the api

---
+++ YelpAgainstCompanies.Angular/YelpAgainstCompanies/src/app/services/company-service.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Observable, catchError, of } from 'rxjs';
+import { CustomHttpClientService } from './custom-http-client.service';
+import Company from '../interfaces/company';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class CompanyServiceService {
+
+  constructor(private customHttpClient: CustomHttpClientService) { }
+
+  getCompanies(): Observable<Company[]> {
+    const url: string = "/company/companies";
+
+    return this.customHttpClient.get<Company[]>(url);
+  }
+}
