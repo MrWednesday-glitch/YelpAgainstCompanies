06448c756dee6ed8612064e63ca8992e819ca50f
Rowan Chander <rowan.chander@itvitaelearning.nl>
2023-10-10 14:47:51 +0200

    Set up the classes for extra tests

---
+++ YelpAgainstCompanies.Business.Testing/ExtensionTests/PasswordExtensionTests/IsValidPasswordTests.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YelpAgainstCompanies.Business.Testing.ExtensionTests.PasswordExtensionTests
+{
+    internal class IsValidPasswordTests
+    {
+    }
+}
