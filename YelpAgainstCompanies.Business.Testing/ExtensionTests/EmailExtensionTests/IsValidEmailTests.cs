8ae2db703fa0a0de85dbae5104c91661db20283f
Rowan Chander <rowan.chander@itvitaelearning.nl>
2023-10-10 15:04:12 +0200

    Wrote unit tests

--- YelpAgainstCompanies.Business.Testing/ExtensionTests/EmailExtensionTests/IsValidEmailTests.cs
+++ YelpAgainstCompanies.Business.Testing/ExtensionTests/EmailExtensionTests/IsValidEmailTests.cs
@@ -1,12 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace YelpAgainstCompanies.Business.Testing.ExtensionTests.EmailExtensionTests;
 
-namespace YelpAgainstCompanies.Business.Testing.ExtensionTests.EmailExtensionTests
+[ExcludeFromCodeCoverage]
+public class IsValidEmailTests
 {
-    internal class IsValidEmailTests
+    [Theory]
+    [InlineData(true, "rowan@email.com")]
+    [InlineData(true, "rowan@email.nl")]
+    [InlineData(false, "rowan.email.com")]
+    [InlineData(false, "rowan@emailcom")]
+    public void Should_CheckEmailsCorrectly(bool expected, string email)
     {
+        // -- Arrange
+
+        // -- Act
+        var sut = email.IsValidEmail();
+
+        // -- Assert
+        sut.Should().Be(expected);
     }
 }
--- YelpAgainstCompanies.Business.Testing/ExtensionTests/PasswordExtensionTests/IsValidPasswordTests.cs
+++ YelpAgainstCompanies.Business.Testing/ExtensionTests/PasswordExtensionTests/IsValidPasswordTests.cs
@@ -1,12 +1,22 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace YelpAgainstCompanies.Business.Testing.ExtensionTests.PasswordExtensionTests;
 
-namespace YelpAgainstCompanies.Business.Testing.ExtensionTests.PasswordExtensionTests
+[ExcludeFromCodeCoverage]
+public class IsValidPasswordTests
 {
-    internal class IsValidPasswordTests
+    [Theory]
+    [InlineData(true, "ANGelo55!!")]
+    [InlineData(true, "BatMan(4)")]
+    [InlineData(false, "abrakadabra4!")]
+    [InlineData(false, "aBRAkadabra4")]
+    [InlineData(false, "aBRAkadabra!!")]
+    public void Should_CheckPasswordsCorrectly(bool expected, string password)
     {
+        // -- Arrange
+
+        // -- Act
+        var sut = password.IsValidPassword();
+
+        // -- Assert
+        sut.Should().Be(expected);
     }
 }
