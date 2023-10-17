18b3ecd89e4aa1c500b95f458af98a5d62568dba
Rowan Chander <rowan.chander@itvitaelearning.nl>
2023-10-17 14:17:17 +0200

    Generated the form component

---
+++ YelpAgainstCompanies.Angular/YelpAgainstCompanies/src/app/components/add-company-form/add-company-form.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-company-form',
+  templateUrl: './add-company-form.component.html',
+  styleUrls: ['./add-company-form.component.scss']
+})
+
+export class AddCompanyFormComponent {
+
+}
