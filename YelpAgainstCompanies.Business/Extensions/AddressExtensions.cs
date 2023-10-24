70ec0fcb8d9d06331a8bda4993541c20062f93db
Rowan Chander <chander89@live.nl>
2023-10-24 13:31:39 +0200

    Added validators

---
+++ YelpAgainstCompanies.Business/Extensions/AddressExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace YelpAgainstCompanies.Business.Extensions;
+
+public static class AddressExtensions
+{
+    public static bool IsValidAddress(this string address)
+    {
+        var regexPattern = "^.+\\s{1}[0-9]+\\w{0,2}$";
+
+        return Regex.IsMatch(address, regexPattern);
+    }
+
+    public static bool IsValidPostalCode(this string postalCode)
+    {
+        var regexPattern = "^[1-9][0-9]{3} ?(?!sa|sd|ss)[a-z]{2}$";
+
+        return Regex.IsMatch(postalCode, regexPattern);
+    }
+}
