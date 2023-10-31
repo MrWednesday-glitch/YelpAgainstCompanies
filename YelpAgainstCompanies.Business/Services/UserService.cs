d14894ddf639f0e83b8443d3fbfec923bb49bad8
Rowan Chander <rowan.chander@itvitaelearning.nl>
2023-10-31 16:02:08 +0100

    A todo

--- YelpAgainstCompanies.Business/Services/UserService.cs
+++ YelpAgainstCompanies.Business/Services/UserService.cs
@@ -9,6 +9,7 @@ public class UserService : IUserService
         _userRepository = userRepository;
     }
 
+    //TODO TEst this
     public AppUser GetUser(string username)
     {
         return _userRepository.GetUser(username);
