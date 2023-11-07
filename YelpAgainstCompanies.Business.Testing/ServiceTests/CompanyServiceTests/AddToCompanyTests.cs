a8a698c06f65517db9765964dd4597422724968b
Rowan Chander <rowan.chander@itvitaelearning.nl>
2023-11-07 12:24:26 +0100

    Two more tests

--- YelpAgainstCompanies.Business.Testing/ServiceTests/CompanyServiceTests/AddToCompanyTests.cs
+++ YelpAgainstCompanies.Business.Testing/ServiceTests/CompanyServiceTests/AddToCompanyTests.cs
@@ -38,4 +38,67 @@ public class AddToCompanyTests : Base
         // -- Assert
         updatedCompany.Ratings.ToList().Count.Should().Be(2);
     }
+
+    [Fact]
+    public async Task Should_AverageAccurately()
+    {
+        // -- Arrange
+        var newRating = new Rating
+        {
+            Date = DateTime.Now,
+            Comment = "I'm a comment.",
+            CompanyId = 1,
+            Score = 3,
+        };
+        var company = new Company
+        {
+            Id = 1,
+            Name = "AH",
+            Score = 4,
+            Ratings = new List<Rating>
+            {
+                new Rating
+                {
+                    Date = new DateTime(2023, 9, 4),
+                    Comment = "I'm an old comment.",
+                    CompanyId = 1,
+                    Score = 4,
+                }
+            }
+        };
+        _mockedCompanyRepository.Setup(x => x.GetRecord(1)).Returns(company);
+
+        // -- Act
+        var updatedCompany = await _companyService.AddToCompany(newRating);
+
+        // -- Assert
+        updatedCompany.Score.Should().Be(3.5);
+    }
+
+    [Fact]
+    public async Task Should_AverageAccuratelyWhenFirstComment()
+    {
+        // -- Arrange
+        var newRating = new Rating
+        {
+            Date = DateTime.Now,
+            Comment = "I'm a comment.",
+            CompanyId = 1,
+            Score = 3,
+        };
+        var company = new Company
+        {
+            Id = 1,
+            Name = "AH",
+            Score = 4,
+            Ratings = new List<Rating>()
+        };
+        _mockedCompanyRepository.Setup(x => x.GetRecord(1)).Returns(company);
+
+        // -- Act
+        var updatedCompany = await _companyService.AddToCompany(newRating);
+
+        // -- Assert
+        updatedCompany.Score.Should().Be(3);
+    }
 }
