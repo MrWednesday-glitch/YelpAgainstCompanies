314cd151a80fd6c2d302762124b700160a270b6e
Rowan Chander <rowan.chander@itvitaelearning.nl>
2023-11-07 13:11:39 +0100

    Api call, all the way through to the database, works. Tested via postman

--- YelpAgainstCompanies.Api/Controllers/CompanyController.cs
+++ YelpAgainstCompanies.Api/Controllers/CompanyController.cs
@@ -1,4 +1,6 @@
-﻿namespace YelpAgainstCompanies.Api.Controllers;
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace YelpAgainstCompanies.Api.Controllers;
 
 [ApiController]
 [Route("company")]
@@ -6,11 +8,13 @@ public class CompanyController : Controller
 {
     private readonly ICompanyService _companyService;
     private readonly Transformations _transformations;
+    private readonly IUserService _userService;
 
-    public CompanyController(ICompanyService companyService, Transformations transformations)
+    public CompanyController(ICompanyService companyService, Transformations transformations, IUserService userService)
     {
         _companyService = companyService;
         _transformations = transformations;
+        _userService = userService;
     }
 
     [HttpGet("companies")]
@@ -75,5 +79,62 @@ public class CompanyController : Controller
                 Success = false
             });
         }
+    }
+
+    [Authorize]
+    [HttpPost("attachratingtocompany/{companyId}")]
+    public async Task<IActionResult> AttachRatingToCompany([FromBody] MadeRating madeRating, int companyId)
+    {
+        try
+        {
+            JwtSecurityToken bearerToken = GetBearerToken() ?? throw new Exception("Cannot add comment when not logged in.");
+            string userName = bearerToken.Claims.Single(c => c.Type == "Username" /*ClaimTypes.Name*/).Value;
+            var user = _userService.GetUser(userName);
+
+            var rating = new Rating
+            {
+                Comment = madeRating.Comment ?? "Rater did not add a comment.",
+                Date = DateTime.Now,
+                CompanyId = companyId,
+                Score = madeRating.Score,
+                //Maybe do this in the service?
+                User = user,
+                UserId = user.Id,
+            };
+
+            var updateCompany = await _companyService.AddToCompany(rating);
+            var companyAndRatingsDTO = _transformations.Transform(updateCompany, updateCompany.Ratings);
+
+            return Ok(new
+            {
+                Message = $"The rating has been added.",
+                Company = companyAndRatingsDTO,
+                Success = true
+            });
+        }
+        catch (Exception ex)
+        {
+            return BadRequest(new
+            {
+                Message = ex.Message,
+                Success = false
+            });
+        }
+    }
+
+    private JwtSecurityToken? GetBearerToken()
+    {
+        IEnumerable<string> authValues = Request.Headers.Authorization;
+
+        if (authValues.Any())
+        {
+            string[] token = authValues.First().Split(' ');
+            if (token.Length == 2 && token.First() == "Bearer")
+            {
+                return new JwtSecurityTokenHandler().ReadJwtToken(token.Last());
+            }
+        }
+
+        return null;
     }
 }
--- YelpAgainstCompanies.Api/Controllers/RatingController.cs
+++ YelpAgainstCompanies.Api/Controllers/RatingController.cs
@@ -1,6 +1,4 @@
-﻿using System.IdentityModel.Tokens.Jwt;
-
-namespace YelpAgainstCompanies.Api.Controllers;
+﻿namespace YelpAgainstCompanies.Api.Controllers;
 
 [ApiController]
 [Route("rating")]
@@ -8,13 +6,11 @@ public class RatingController : Controller
 {
     private readonly IRatingService _ratingService;
     private readonly Transformations _transformations;
-    private readonly IUserService _userService;
 
-    public RatingController(IRatingService ratingService, Transformations transformations, IUserService userService)
+    public RatingController(IRatingService ratingService, Transformations transformations)
     {
         _ratingService = ratingService;
         _transformations = transformations;
-        _userService = userService;
     }
 
     [HttpGet("{companyId}")]
@@ -31,60 +27,5 @@ public class RatingController : Controller
         {
             return NotFound(ex.Message);
         }
-    }
-
-    [Authorize]
-    [HttpPost("attachratingtocompany/{companyId}")]
-    public async Task<IActionResult> AttachRatingToCompany([FromBody] MadeRating madeRating, int companyId)
-    {
-        try
-        {
-            JwtSecurityToken bearerToken = GetBearerToken() ?? throw new Exception("Cannot add comment when not logged in.");
-            string userName = bearerToken.Claims.Single(c => c.Type == ClaimTypes.Name).Value;
-            var user = _userService.GetUser(userName);
-
-            var rating = new Rating
-            {
-                Comment = madeRating.Comment ?? "Rater did not add a comment.",
-                Date = DateTime.Now,
-                CompanyId = companyId,
-                Score = madeRating.Score,
-                //Maybe do this in the ratingservice?
-                User = user,
-                UserId = user.Id,
-            };
-
-            await _ratingService.AddToCompany(rating);
-
-            return Ok(new
-            {
-                Message = $"The rating has been added.",
-                Success = true
-            });
-        }
-        catch (Exception ex)
-        {
-            return BadRequest(new
-            {
-                Message = ex.Message,
-                Success = false
-            });
-        }
-    }
-
-    private JwtSecurityToken? GetBearerToken()
-    {
-        IEnumerable<string> authValues = Request.Headers.Authorization;
-
-        if (authValues.Any())
-        {
-            string[] token = authValues.First().Split(' ');
-            if (token.Length == 2 && token.First() == "Bearer")
-            {
-                return new JwtSecurityTokenHandler().ReadJwtToken(token.Last());
-            }
-        }
-
-        return null;
     }
 }
