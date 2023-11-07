b4abb36dee0d7e96788e66799a0ba4b491a7bac8
Rowan Chander <rowan.chander@itvitaelearning.nl>
2023-11-07 15:00:40 +0100

    Made the add rating component

---
+++ YelpAgainstCompanies.Angular/YelpAgainstCompanies/src/app/components/add-rating-form/add-rating-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-rating-form',
+  templateUrl: './add-rating-form.component.html',
+  styleUrls: ['./add-rating-form.component.scss']
+})
+export class AddRatingFormComponent {
+
+}
